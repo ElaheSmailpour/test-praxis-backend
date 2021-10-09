c502b2d03044634be52104b5290430d688bc179f
ElaheSmailpour <eli_es87@yahoo.com>
2021-10-09 12:36:24 +0200

    login

--- controller/userController.js
+++ controller/userController.js
@@ -18,3 +18,70 @@ exports.signup = async (req, res, next) => {
         res.status(500).send('Something went wrong!')
     }
 }
+exports.login = async (req, res, next) => {
+    let loginuser = req.body
+
+    try {
+
+        let useremail = await User.findOne({ email: loginuser.email })
+        console.log(useremail);
+
+        if (useremail === null) {
+            return res.status(401).send('incorrect email')
+        }
+
+        let comparePasswort = await bcrypt.compare(loginuser.password, useremail.password)
+
+        if (comparePasswort) {
+
+            let token = jwt.sign({
+                email: useremail.email,
+                userId: useremail._id,
+            }, process.env.JWT || 'secret', { expiresIn: '2h' })
+            res.status(200).json({
+                message: 'You are log it',
+                token: token,
+                name: useremail.name
+
+
+            })
+
+        }
+        else {
+            res.status(401).send('You are  not log it')
+        }
+    }
+    catch (error) {
+        res.status(401).send('Du konntest nicht eingeloggt werden')
+    }
+}
+
+
+
+
+
+
+//post-signup
+
+exports.getSignup = async (req, res, next) => {
+
+    try {
+        const newuser = req.body
+
+
+        let alreadyuser = await User.find({ $or: [{ email: newuser.email }, { phone: newuser.phone }] })
+        if (alreadyuser.length >= 1) {
+            return res.status(409).send('There is already a user with this email or phone')
+        }
+
+        let passwortGehashed = await bcrypt.hash(newuser.password, 10)
+        let createuser = await User.create({ ...newuser, password: passwortGehashed })
+
+
+        res.status(201).send(createuser);
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Something went wrong!')
+    }
+}
